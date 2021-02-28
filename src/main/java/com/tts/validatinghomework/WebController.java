b7fd1ce4769b32dccec1784d57be9d427ce96918
sallymatos <sally.matos322@gmail.com>
2021-02-28 17:51:16 -0600

    Initial commit

---
+++ src/main/java/com/tts/validatinghomework/WebController.java
@@ -0,0 +1,36 @@
+package com.tts.validatinghomework;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Controller
+public class WebController implements WebMvcConfigurer {
+
+    // this is for creating the result page to display
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/results").setViewName("results");
+    }
+
+    @GetMapping("/")
+    public String showForm(PersonForm personForm) {
+        return "form";
+    }
+
+    @PostMapping("/")
+    //check for information for validity and show result
+    public String checkPersonInfo(@Valid PersonForm personForm, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "form";
+        }
+
+        return "redirect:/results";
+    }
+}
---
+++ src/test/java/com/tts/validatinghomework/ValidatingHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.validatinghomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidatingHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
